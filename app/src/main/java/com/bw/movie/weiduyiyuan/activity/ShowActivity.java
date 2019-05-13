ced0c933258e2eb172bdbc1be25f74dfea84b6cc
ChenNa420 <2415821922@qq.com>
2019-05-13 10:13:01 +0800

    first commit

---
+++ app/src/main/java/com/bw/movie/weiduyiyuan/activity/ShowActivity.java
@@ -0,0 +1,60 @@
+package com.bw.movie.weiduyiyuan.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.bw.movie.weiduyiyuan.R;
+import com.bw.movie.weiduyiyuan.adapter.MyFragmentPagerAdapter;
+import com.bw.movie.weiduyiyuan.fragment.FilmFragment;
+import com.bw.movie.weiduyiyuan.fragment.MyFragment;
+import com.bw.movie.weiduyiyuan.fragment.ShouYeFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ShowActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+    @BindView(R.id.radio_group)
+    RadioGroup radioGroup;
+    List<Fragment> list=new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+        ButterKnife.bind(this);
+        list.add(new ShouYeFragment());
+        list.add(new FilmFragment());
+        list.add(new MyFragment());
+        MyFragmentPagerAdapter adapter=new MyFragmentPagerAdapter(getSupportFragmentManager(),list);
+        viewPager.setAdapter(adapter);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.btn_rb1:
+                        viewPager.setCurrentItem(0,false);
+                        break;
+                    case R.id.btn_rb2:
+                        viewPager.setCurrentItem(1,false);
+                        break;
+                    case R.id.btn_rb3:
+                        viewPager.setCurrentItem(2,false);
+                        break;
+                }
+            }
+        });
+
+    }
+}
